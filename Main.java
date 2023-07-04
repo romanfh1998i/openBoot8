8c0d243739da7a33b5ff770b7addfb58dce3ba22
romanfh1998i <44349888+romanfh1998i@users.noreply.github.com>
2023-07-04 19:01:44 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    Persona persona = new Persona();
+
+    persona.setEdad(25);
+    persona.setNombre("Juan");
+    persona.setTelefono("123456");
+  
+
+    
+   
+  
+    System.out.println("edad "+persona.getEdad());
+        System.out.println("Nombre "+persona.getNombre());
+        System.out.println("Telefono "+persona.getTelefono());
+  }
+}
